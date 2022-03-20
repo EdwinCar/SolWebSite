0d31d6cbfcd881ff04a825c1db340d25c42f096a
Edwin Higberto Mamani Canaza <sobrenet.develop@gmail.com>
2022-03-20 16:11:45 -0400

    Snippet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [];
 
+/**
+* Created by S73261 [Edwin Higberto Mamani Canaza] on 20/03/2022.
+**/
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+/**
+* Created by S73261 [Edwin Higberto Mamani Canaza] on 20/03/2022.
+**/
 @NgModule({
   declarations: [
     AppComponent
